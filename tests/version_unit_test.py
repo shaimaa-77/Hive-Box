85a9cc90fe192ecea718a1f424e72b460cbd271c
shaimaa-turky <shaimaatorky77@gmail.com>
2025-01-09 04:59:18 +0200

    Continuous Integration Create a GitHub Actions workflow for CI. Add step to lint code and Dockerfile. Add step to build the Docker image. Add step to unit tests. Setup OpenSSF Scorecard GitHub Action and fix any issues reported by it.

---
+++ tests/version_unit_test.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from main import app
+import httpx
+client=TestClient(app)
+
+def test_version_endpoint():
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert "version" in response.json()
+    assert response.json()["version"] == app.version
