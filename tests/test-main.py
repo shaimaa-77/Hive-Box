05a433dcf2484fb67f1d1fd29c7031170fc3612b
shaimaa-turky <shaimaatorky77@gmail.com>
2025-04-18 23:09:17 +0200

    add test cases

---
+++ tests/test-main.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_app_version():
+    #test version
+    respond=client.get("/version")
+    assert respond.status_code == 200
+    assert "version" in respond.json
+
+
+def test_app_temperature():
+    #test temperature
+    respond=client.get("/temperature")
+    assert respond.status_code == 200
+    assert "average_temperatures" in respond.json
